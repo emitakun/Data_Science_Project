2e3034810d04a0d4217b585ad78ffd0143923fa5
emitakun <73317419+emitakun@users.noreply.github.com>
2025-08-23 05:03:54 +0100

    Update 6g_iot_beamforming_dataset_csv.py

--- 6g_iot_beamforming_dataset_csv.py
+++ 6g_iot_beamforming_dataset_csv.py
@@ -61,6 +61,7 @@ plt.xlabel('Number of Antennas')
 plt.ylabel('Beamforming Gain (dB)')
 plt.title('Number of Antennas vs. Beamforming Gain (dB)')
 plt.show()
+st.pyplot(fig)
 
 corr = df.corr()
 plt.figure(figsize=(14, 10))
