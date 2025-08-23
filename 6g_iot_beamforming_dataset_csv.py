eb35ab049a93df2f73cc8d788351f698aa9f92b5
emitakun <73317419+emitakun@users.noreply.github.com>
2025-08-23 03:43:59 +0100

    Update 6g_iot_beamforming_dataset_csv.py

--- 6g_iot_beamforming_dataset_csv.py
+++ 6g_iot_beamforming_dataset_csv.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-#import streamlit as st
+import streamlit as st
 import io
 from sklearn.model_selection import train_test_split
 
