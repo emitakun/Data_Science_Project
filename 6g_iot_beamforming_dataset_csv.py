9e88ff869f6eb0b6b545bb17f8c52192d8c83396
emitakun <73317419+emitakun@users.noreply.github.com>
2025-08-23 04:56:39 +0100

    Update 6g_iot_beamforming_dataset_csv.py

--- 6g_iot_beamforming_dataset_csv.py
+++ 6g_iot_beamforming_dataset_csv.py
@@ -124,3 +124,10 @@ st.download_button(
 )
 st.sidebar.header("About")
 st.sidebar.info("This application estimates if beamforming is optimized or not in a 6G signal")
+input_data{}
+for x in x.columns:
+    if df[col].dtype in [np.int64, np.float64]:
+        input_data[col] = st.number_input(f"{col}", value = float(df[col].mean()))
+    else:
+        input_data[col] = st.text_input(f"{col}")
+input_df = pd.DataFrame([input_data])
